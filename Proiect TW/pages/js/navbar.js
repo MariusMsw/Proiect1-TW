d5c1cdd03ed85fa33c077ba5c39fe1060666e30d
MariusMsw <38507154+MariusMsw@users.noreply.github.com>
2019-10-29 20:18:36 +0200

    first commit

---
+++ Proiect TW/pages/js/navbar.js
@@ -0,0 +1,10 @@
+  function myFunction() {
+  var x = document.getElementById("myDropdown");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    x.style.opacity = "0";
+  } else {
+    x.style.display = "block";
+    x.style.opacity = "1";
+  }
+} 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Proiect1-TW
+
+link video: https://daks2k3a4ib2z.cloudfront.net/5983124b9d906600016bfc79/5a563bebc8be69000197f760_Aviatiei%20Park_1-transcode.webm
